33ab92e854d1d0b29f00281329de41bc9d7334c4
Timothy Stephens <timothy.f.stephens@gmail.com>
2018-02-01 09:12:52 -0500

    Formatting events

--- src/app/modules/event/event.model.ts
+++ src/app/modules/event/event.model.ts
@@ -2,7 +2,11 @@ import { Animal } from "../api";
 
 export class EventModel {
     public id: number;
-    public start: Date;
-    public end: Date;
-    public animals: Animal[];
+    public startTime: Date;
+    public endTime: Date;
+    public __animals__: Animal[];
+
+    get animals(): Animal[] {
+        return this.__animals__;
+    }
 }
--- src/app/modules/event/index/index.page.ts
+++ src/app/modules/event/index/index.page.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from "@angular/core";
 
-import { Observable } from "rxjs";
+import * as moment from "moment";
+import "twix";
+import { Observable, BehaviorSubject } from "rxjs";
 
 import { EventService } from "../event.service";
 
@@ -12,9 +14,29 @@ import { EventModel } from "../event.model";
 })
 export class EventIndexPage {
 
-    public events: Observable<EventModel[]>;
+    public events: BehaviorSubject<EventModel[]>;
 
     constructor(eventService: EventService) {
-        this.events = eventService.getEvents();
+        this.events = new BehaviorSubject<EventModel[]>([]);
+        eventService.getEvents().subscribe((events: EventModel[]) => {
+            this.events.next(events.sort((a: EventModel, b: EventModel) => {
+                let mAs = moment(a.startTime);
+                let mBs = moment(b.startTime);
+                let mAe = moment(a.endTime);
+                let mBe = moment(b.endTime);
+
+                if (mAs.isBefore(mBs) || (mAs.isSame(mBs) && mAe.isBefore(mBe))) {
+                    return -1;
+                } else if (mAs.isSame(mBs) && mAs.isSame(mBe)) {
+                    return 0;
+                }
+
+                return 1;
+            }));
+        });
+    }
+
+    formatEventDate(event: EventModel): string {
+        return moment(event.startTime).twix(event.endTime).format({implicitMinutes: false});
     }
 }
